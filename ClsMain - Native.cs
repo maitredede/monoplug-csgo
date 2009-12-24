9ca17dd3ccbb56d01ffe4bedd3f50f2c6da4d1f7
MaitreDede <MaitreDede@68180c0e-ab10-4640-99c0-d1b6c53dfd26>
2009-12-24 15:08:05 +0000

    git-svn-id: https://xp-dev.com/svn/MonoPlug/trunk@10 68180c0e-ab10-4640-99c0-d1b6c53dfd26

--- ClsMain - Native.cs
+++ ClsMain - Native.cs
@@ -8,7 +8,7 @@ namespace MonoPlug
 		[MethodImpl(MethodImplOptions.InternalCall)]
 		private static extern void Mono_Msg(string msg);
 		[MethodImpl(MethodImplOptions.InternalCall)]
-		private static extern bool Mono_RegisterConCommand(string name, string description, ConCommandDelegate code);
+		private static extern bool Mono_RegisterConCommand(string name, string description, Delegate code);
 		[MethodImpl(MethodImplOptions.InternalCall)]
 		private static extern bool Mono_UnregisterConCommand(string name);
 	}
