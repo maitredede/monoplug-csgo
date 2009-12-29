03cc25e249b3e25b4722484d8438c4437d5adaab
MaitreDede <MaitreDede@68180c0e-ab10-4640-99c0-d1b6c53dfd26>
2009-12-29 14:58:42 +0000

    git-svn-id: https://xp-dev.com/svn/MonoPlug/trunk@54 68180c0e-ab10-4640-99c0-d1b6c53dfd26

---
+++ Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Test
+{
+    delegate void d(string str);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(d);
+            foreach (MethodInfo m in typeof(Program).GetMethods(BindingFlags.Static | BindingFlags.Public))
+            {
+                try
+                {
+                    Delegate.CreateDelegate(t, m);
+                }
+                catch (ArgumentException)
+                {
+                }
+            }
+        }
+
+        public static void tata(string str)
+        {
+        }
+
+        public static string toto(string str)
+        {
+            return str;
+        }
+    }
+}
